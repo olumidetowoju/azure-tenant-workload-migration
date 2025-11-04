186c1da81fa0f56a736617cc0399412ae1bdb7f7
Olumide Towoju <230638625+olumidetowoju@users.noreply.github.com>
2025-11-04 14:11:25 -0500

    Added CLI variable file for Day 1 environment

---
+++ scripts/cli/vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Azure Tenant Workload Migration - Environment Variables
+# Author: Olumide Towoju
+# Region: East US
+# Subscription: Free Tier
+
+export LOCATION="eastus"
+export SOURCE_TENANT="fabrikam.onmicrosoft.com"
+export TARGET_TENANT="contoso.onmicrosoft.com"
+export RG_SOURCE="rg-01-eus-src"
+export RG_TARGET="rg-01-eus-tgt"
+export OWNER="olumidetowoju"
+export LAB="day01"
+export SCENARIO="acquisition"
